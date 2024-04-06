718fc2db067433e521466a7ee9b71048834e2201
softdev1012 <matthewjin1012@gmail.com>
2024-04-06 18:59:40 +0200

    tab

--- backend/src/models/Referral.ts
+++ backend/src/models/Referral.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { IReferral } from '../types/referral';
 
 const referralSchema = new mongoose.Schema({
-groupname: String,
+  groupname: String,
   fullname: String,
   phone: String,
   from: String,
